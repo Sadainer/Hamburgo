9b5afd5aa2cd1cbfe4bd10e0bec30098661b6b5f
Sadainer Hernandez <sadainer@hotmail.com>
2016-07-12 16:48:12 -0500

    Inicial

--- app/src/main/java/com/hamburgo/tecnoparque/hamburgo/NuevoClienteDialogFragment.java
+++ app/src/main/java/com/hamburgo/tecnoparque/hamburgo/NuevoClienteDialogFragment.java
@@ -28,7 +28,7 @@ public class NuevoClienteDialogFragment extends android.app.DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        
+
 
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
