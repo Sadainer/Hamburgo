0cbbc428fa02b277e6ef8b2e4f611d7cae9369ae
sadainer <sadainer@hotmail.com>
2016-08-22 15:53:36 -0500

    Vender terminado

---
+++ app/src/main/java/com/hamburgo/tecnoparque/hamburgo/LlenarBaseDatosPrueba.java
@@ -0,0 +1,59 @@
+package com.hamburgo.tecnoparque.hamburgo;
+
+import android.content.Context;
+
+import com.hamburgo.tecnoparque.hamburgo.DAL.DataBaseManager;
+import com.hamburgo.tecnoparque.hamburgo.DTO.ProductoDTO;
+
+import java.util.ArrayList;
+
+/**
+ * Created by YOLIMA on 22/08/2016.
+ */
+public class LlenarBaseDatosPrueba {
+   DataBaseManager manager;
+
+
+    public void Llenar(Context cnt){
+        manager = new DataBaseManager(cnt);
+
+        ProductoDTO a = new ProductoDTO();
+        a.setNombre("TV Samsung 48");
+        a.setTipo("Televisor");
+        a.setPrecio(1200000);
+        a.setDescripcion("Smart TV");
+        manager.InsertarProductos(a);
+
+        ProductoDTO b = new ProductoDTO();
+        b.setNombre("TV Samsung 46");
+        b.setTipo("Televisor");
+        b.setPrecio(1000000);
+        b.setDescripcion("Smart TV");
+        manager.InsertarProductos(b);
+
+        ProductoDTO c = new ProductoDTO();
+        c.setNombre("TV LG 40");
+        c.setTipo("Televisor");
+        c.setPrecio(800000);
+        c.setDescripcion("Smart TV");
+        manager.InsertarProductos(c);
+
+        ProductoDTO d = new ProductoDTO();
+        d.setNombre("Reloj Tissot");
+        d.setTipo("Reloj");
+        d.setPrecio(1800000);
+        d.setDescripcion("Reloj");
+        manager.InsertarProductos(d);
+
+        ProductoDTO e = new ProductoDTO();
+        e.setNombre("Bolso Nicol Lee");
+        e.setTipo("Bolsos");
+        e.setPrecio(200000);
+        e.setDescripcion("Bolsos");
+        manager.InsertarProductos(e);
+
+
+
+
+    }
+}
